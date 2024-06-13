711fee5c07027be5d7eb1825783bee3c086e98d6
Лилиана Кукенова <you@example.com>
2024-06-13 18:26:30 +0600

    clear code commit

--- src/main/java/kg/ab/todolist/commons/enums/StatusOfTask.java
+++ src/main/java/kg/ab/todolist/commons/enums/StatusOfTask.java
@@ -1,7 +1,5 @@
 package kg.ab.todolist.commons.enums;
 
-import jakarta.persistence.JoinColumn;
-
 public enum StatusOfTask {
     COMPLETED,
     NOT_COMPLETED;
--- src/main/java/kg/ab/todolist/dto/TaskNameDto.java
+++ src/main/java/kg/ab/todolist/dto/TaskNameDto.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import kg.ab.todolist.commons.enums.StatusOfTask;
 import lombok.Builder;
 
-@Builder
 @Schema(description = "Сущность задачи")
 public record TaskNameDto(@JsonProperty("task_name") @Schema(description = "Название задачи") String taskName,
                           @JsonProperty("status") @Schema(description = "Статус задачи", example = "COMPLETED") StatusOfTask status) {
