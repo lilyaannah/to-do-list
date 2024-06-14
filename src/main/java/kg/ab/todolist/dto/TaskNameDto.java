ef8bed74490697bb075e4da0fac3f08bdb332c7f
Лилиана Кукенова <you@example.com>
2024-06-14 10:00:09 +0600

    builder

--- src/main/java/kg/ab/todolist/dto/TaskNameDto.java
+++ src/main/java/kg/ab/todolist/dto/TaskNameDto.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import kg.ab.todolist.commons.enums.StatusOfTask;
 import lombok.Builder;
 
+@Builder
 @Schema(description = "Сущность задачи")
 public record TaskNameDto(@JsonProperty("task_name") @Schema(description = "Название задачи") String taskName,
                           @JsonProperty("status") @Schema(description = "Статус задачи", example = "COMPLETED") StatusOfTask status) {
